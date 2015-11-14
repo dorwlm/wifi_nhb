892b3865d8c0bfa3a4af7ca9ad36b96067b386ed
root <dorwlm@gmail.com>
2015-11-14 15:13:15 -0500

    scapy monitor APs

--- Clnt/autonic.py
+++ Clnt/autonic.py
@@ -37,10 +37,9 @@ def find_wireless_nic():
             print "value is monitor"
     '''
     #try to find Wireless NIC that already configured as Monitor
-
-    i=0
-    found=False
-    interface=""
+    i = 0
+    found = False
+    interface = None
     while(i<len(interfaces) and not found):
         interface = interfaces.keys()[i]
         print "checking interface: " + str(interface)
@@ -52,19 +51,15 @@ def find_wireless_nic():
                 found = True
         i = i + 1
 
-    print "will return " + str(interface)
-    if(found): return interface
-    else: "will try to configure interface..."
-
     #try to configure interface card from the pool
-    i=0
+    i = 0
     configured = False
-    while(i<len(interfaces) and not configured):
+    while(not found and i<len(interfaces) and not configured):
         interface = interfaces.keys()[i]
         print "checking interface: " + str(interface)
         #check more mode options
         #check error raises
-        if 'Monitor' not in interfaces[interface].keys() and not interfaces[interface]['Managed']==0:
+        if 'Monitor' not in interfaces[interface].keys() and not interfaces[interface]['Managed'] == 0:
             #change configuration
             print "will disable interface"
             call(["ifconfig",str(interface), "down"])
@@ -72,27 +67,31 @@ def find_wireless_nic():
                 Wireless(interface).setMode('Monitor')
             except ValueError:
                 print "resource is busy"
-
             print "will enable interface"
             call(["ifconfig",str(interface), "up"])
             if Wireless(interface).getMode()== 'Monitor':
                 configured=True
             else:
-                interfaces[interface]['Managed']=0
+                interfaces[interface]['Managed'] = 0
         else:
-            interfaces[interface]['Monitor']=0
-        i=i+1
+            interfaces[interface]['Monitor'] = 0
+        i = i + 1
+
+    print "will return " + str(interface)
+    if(found or configured): return interface
+    else: return None
 
 
 if __name__ == "__main__":
 
     parser = OptionParser(usage="%prog [-i] <Interface> [-v] [-l]", version="%prog 1.0")
-    parser.add_option("-i", "--interface", type="string", dest="interface", default="",
+    parser.add_option("-i", "--interface", type="string", dest="interface", default=None,
                       help="Specify wireless interface card to work with, otherwise will try automatically",
                       metavar="Interface")
     parser.add_option("-v", "--verbose", default=False, action="store_true", dest="verbose",
                       help="Verbose, quiet by default")
-    parser.add_option("-l", "--list", action="store_true", default=False, dest="list", help="List available Wireless NICS")
+    parser.add_option("-l", "--list", action="store_true", default=False, dest="list",
+                      help="List available Wireless NICS")
 
     (options, args) = parser.parse_args()
 
@@ -111,7 +110,8 @@ if __name__ == "__main__":
         #automatically mode
         print "Automatic interface mode will being operate"
         interface = find_wireless_nic()
-        print "returned interface: " + str(interface)
-        #if not working correctly should change status to 0
-        print "try to use this interface if not working blame dorwlm"
+        if interface: print "returned interface: " + str(interface)
+        else:
+            #if not working correctly should change status to 0
+            print "try to use another interface if not working blame dorwlm"
 
